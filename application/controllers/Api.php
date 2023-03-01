adf8d7c52cf39d0614c67d01a7262a42eec5ba2c
omartins-zs <gabrielmatheus209@gmail.com>
2023-03-01 10:51:34 -0300

    Mostrando visualização do Get_estudantes da API

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -11,6 +11,13 @@ class Api extends CI_Controller
 
 	public function get_estudantes()
 	{
-		var_dump(get_estudantes());
+		// Json_encode -> Converte Objeto PHP para string JSON
+		$data = array(
+			'status' => 200,
+			'data' => json_encode(get_estudantes()),
+			'comentarios' => 'Olá tudo está bem!!!!!',
+		);
+		
+		var_dump($data);
 	}
 }
